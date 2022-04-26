7bbd025ae6c46cfc7080c00d7f0ed090c2375e12
AYKdgreat <dalemoayokunlephilip@gmail.com>
2022-04-26 14:57:31 +0100

    design_.

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 
 $error = $tr_amount = $tr_title = "";
-$true_today =[];
-$true_tomorrow =["stuff"];
+$true_today = [];
+$true_tomorrow = ["stuff"];
+$true_hist = ["yes"];
 
 if (isset($_POST['add-transaction'])) {
   $tr_title = $_POST['tr-title'];
@@ -60,8 +61,8 @@ if (isset($_POST['add-transaction'])) {
         </div>
       </div>
     </div>
-   
-  <div class="body">
+
+    <div class="body">
       <div class="toggle">
         <div class="hist-tab hide">
           History
@@ -73,22 +74,72 @@ if (isset($_POST['add-transaction'])) {
           Income
         </div>
       </div>
-      <div class="hist-list inactive">
-        <ul class="list">
-          <li>
-            <div>
-              <span class="title">li.desc</span>
-              <span class="amount"># 4,000</span>
-              <span class="date">li.date</span>
-              <!-- <span class="edit right"><i class="las la-edit"></i></span>
-              <span class="delete right"><i class="las la-trash"></i></span> -->
-            </div>
-          </li>
-        </ul>
-        <!-- <ul class="list">No expense recorded, add below!</ul> -->
+
+      <div class="hist-list active">
+        <div class="filter">
+          <form method="get" name="history">
+            <label for="month">Select Month: </label><select name="month" class="input-field select">
+              <option value="January">January</option>
+              <option value="January">January</option>
+              <option value="January">January</option>
+              <option value="January">January</option>
+              <option value="January">January</option>
+              <option value="January">January</option>
+            </select></form>
+        </div>
+
+        <span class="date"><?php echo date("Y-m-d", strtotime("today")); ?></span>
+        <?php if (!empty($true_hist)) : ?>
+          <ul class="list">
+            <li class="card-small clearfix">
+              <div>
+                <span class="title">1. li.title <i class="las la-hashtag"></i></span>
+                <span class="amount"># 4,000</span>
+              </div>
+            </li>
+            <li class="card-small clearfix">
+              <div>
+                <span class="title">2. li.title <i class="las la-credit-card"></i>
+
+                </span>
+                <span class="amount"># 4,000</span>
+              </div>
+            </li>
+          </ul>
+        <?php else : ?>
+          <ul class="list">
+            <li class="card-small clearfix">
+              Nothing for today!
+            </li>
+          </ul>
+        <?php endif; ?>
+        <?php if (!empty($true_tomorrow)) : ?>
+          <span class="date"><?php echo date("Y-m-d", strtotime("yesterday")); ?></span>
+          <ul class="list">
+            <li class="card-small clearfix">
+              <div>
+                <span class="title">1. li.title <i class="las la-hashtag"></i></span>
+                <span class="amount"># 4,000</span>
+              </div>
+            </li>
+            <li class="card-small clearfix">
+              <div>
+                <span class="title">2. li.title <i class="las la-credit-card"></i>
+
+                </span>
+                <span class="amount"># 4,000</span>
+              </div>
+            </li>
+          </ul>
+        <?php else : ?>
+          <ul class="list">
+            <li class="card-small clearfix">
+              Nothing for today!
+            </li>
+          </ul>
+        <?php endif; ?>
       </div>
 
-      
       <div class="recent-list active">
         <span class="date">Today - <?php echo date("Y-m-d", strtotime("today")); ?></span>
         <?php if (!empty($true_today)) : ?>
@@ -102,47 +153,49 @@ if (isset($_POST['add-transaction'])) {
             <li class="card-small clearfix">
               <div>
                 <span class="title">2. li.title <i class="las la-credit-card"></i>
-                
-              </span>
-              <span class="amount"># 4,000</span>
-            </div>
-          </li>
-        </ul>
-        <?php else: ?>
-          <ul class="list"><li class="card-small clearfix">
-            Nothing for today!
-          </li> 
-        </ul>
+
+                </span>
+                <span class="amount"># 4,000</span>
+              </div>
+            </li>
+          </ul>
+        <?php else : ?>
+          <ul class="list">
+            <li class="card-small clearfix">
+              Nothing for today!
+            </li>
+          </ul>
         <?php endif; ?>
         <?php if (!empty($true_tomorrow)) : ?>
-      <span class="date">Yesterday - <?php echo date("Y-m-d", strtotime("yesterday")); ?></span>
-      <ul class="list">
-        <li class="card-small clearfix">
-          <div>
-            <span class="title">1. li.title <i class="las la-hashtag"></i></span>
-            <span class="amount"># 4,000</span>
-          </div>
-        </li>
-        <li class="card-small clearfix">
-          <div>
-            <span class="title">2. li.title <i class="las la-credit-card"></i>
-            
-          </span>
-          <span class="amount"># 4,000</span>
-        </div>
-      </li>
-    </ul>
-    <?php else: ?>
-      <ul class="list"><li class="card-small clearfix">
-        Nothing for today!
-      </li> 
-    </ul>
-    <?php endif; ?>
-    
-  </div>
-  
-  <!-- for loop-->
-  <div class="inc-list inactive">
+          <span class="date">Yesterday - <?php echo date("Y-m-d", strtotime("yesterday")); ?></span>
+          <ul class="list">
+            <li class="card-small clearfix">
+              <div>
+                <span class="title">1. li.title <i class="las la-hashtag"></i></span>
+                <span class="amount"># 4,000</span>
+              </div>
+            </li>
+            <li class="card-small clearfix">
+              <div>
+                <span class="title">2. li.title <i class="las la-credit-card"></i>
+
+                </span>
+                <span class="amount"># 4,000</span>
+              </div>
+            </li>
+          </ul>
+        <?php else : ?>
+          <ul class="list">
+            <li class="card-small clearfix">
+              Nothing for today!
+            </li>
+          </ul>
+        <?php endif; ?>
+
+      </div>
+
+      <!-- for loop-->
+      <div class="inc-list inactive">
         <ul class="list">
           <!-- if income -->
           <li>
@@ -151,88 +204,88 @@ if (isset($_POST['add-transaction'])) {
         </ul>
         <!-- else -->
         <!-- <ul class="list">No income recorded, add below!</ul> -->
-        
+
         <!-- <div class="form">
       <form method="post">
         <div class="error"><?php echo $error; ?>
-        <input type="text" name="inc_desc" placeholder="Description" class="title-input" value="<?php echo $inc_desc; ?>">
-        <input type="number" name="inc_amount" placeholder="Amount" class="amt-input" value="<?php echo $inc_amount; ?>">
+        <input type="text" name="inc_desc" placeholder="Description" class="input-field" value="<?php echo $inc_desc; ?>">
+        <input type="number" name="inc_amount" placeholder="Amount" class="input-field" value="<?php echo $inc_amount; ?>">
         <input type="submit" class="add-inc" name="add_inc" value="Add Income">
       </form>
     </div> -->
+      </div>
+    </div>
+    <div class="form" id="modal">
+      <div class="close-modal" onclick="closeModal()">&times;</div>
+      <form method="post" action="" class="input-form" onsubmit="closeModal()" autocomplete="off">
+        <h2 style="text-align: center; margin-bottom:5px;">New Transaction</h1>
+          <div class="error"><?php echo $error . "<br>"; ?></div>
+          <table>
+            <tr>
+              <td class="m-right">
+                <label for="tr-title">Title <span class="error">*</span></label>
+              </td>
+              <td>
+                <input type="text" name="tr-title" placeholder="e.g. data purchase" class="input-field" value="<?php echo $tr_title; ?>">
+              </td>
+            </tr>
+            <tr>
+              <td class="m-right">
+                <label for="tr-amount">Amount <span class="error">*</span></label>
+              </td>
+              <td>
+                <input type="number" name="tr-amount" placeholder="2500" class="input-field" value="<?php echo $tr_amount; ?>">
+              </td>
+              </td>
+            </tr>
+            <tr>
+              <td class="m-right">
+                <label for="tr-type">Type <span class="error">*</span></label>
+              </td>
+              <td>
+                <select name="tr-type" class="input-field">
+                  <option value="income">Income</option>
+                  <option value="expense">Expense</option>
+                </select>
+              </td>
+              </td>
+            </tr>
+            <tr>
+              <td class="m-right">
+                <label for="tr-type">Date <span class="error">*</span></label>
+              </td>
+              <td>
+                <input type="date" name="tr-date" class="input-field">
+              </td>
+              </td>
+            </tr>
+            </tr>
+            <tr>
+              <td class="m-right">
+                <label for="tr-payment-option">Payment Option <span class="error">*</span></label>
+              </td>
+              </td>
+              <td>
+                <select name="tr-payment-option" class="input-field">
+                  <option value="cash">Cash</option>
+                  <option value="card">Card</option>
+                </select>
+              </td>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <input type="submit" class="add-transaction" name="add-transaction" value="Add Transaction">
+              </td>
+            </tr>
+          </table>
+      </form>
+    </div>
   </div>
-</div>
-<div class="form" id="modal">
-  <div class="close-modal" onclick="closeModal()">&times;</div>
-  <form method="post" action="" class="input-form" onsubmit="closeModal()" autocomplete="off">
-    <h2 style="text-align: center; margin-bottom:5px;">New Transaction</h1>
-      <div class="error"><?php echo $error . "<br>"; ?></div>
-      <table>
-        <tr>
-          <td class="m-right">
-            <label for="tr-title">Title <span class="error">*</span></label>
-          </td>
-          <td>
-            <input type="text" name="tr-title" placeholder="e.g. data purchase" class="title-input" value="<?php echo $tr_title; ?>">
-          </td>
-        </tr>
-        <tr>
-          <td class="m-right">
-            <label for="tr-amount">Amount <span class="error">*</span></label>
-          </td>
-          <td>
-            <input type="number" name="tr-amount" placeholder="2500" class="amt-input" value="<?php echo $tr_amount; ?>">
-          </td>
-          </td>
-        </tr>
-        <tr>
-          <td class="m-right">
-            <label for="tr-type">Type <span class="error">*</span></label>
-          </td>
-          <td>
-            <select name="tr-type" class="amt-input">
-              <option value="income">Income</option>
-              <option value="expense">Expense</option>
-            </select>
-          </td>
-          </td>
-        </tr>
-        <tr>
-          <td class="m-right">
-            <label for="tr-type">Date <span class="error">*</span></label>
-          </td>
-          <td>
-            <input type="date" name="tr-date" class="amt-input">
-          </td>
-          </td>
-        </tr>
-        </tr>
-        <tr>
-          <td class="m-right">
-            <label for="tr-payment-option">Payment Option <span class="error">*</span></label>
-          </td>
-          </td>
-          <td>
-            <select name="tr-payment-option" class="amt-input">
-              <option value="cash">Cash</option>
-              <option value="card">Card</option>
-            </select>
-          </td>
-          </td>
-        </tr>
-        <tr>
-          <td colspan="2">
-            <input type="submit" class="add-transaction" name="add-transaction" value="Add Transaction">
-          </td>
-        </tr>
-      </table>
-  </form>
-</div>
-</div>
 
 
-<button class="open-modal" onclick="openModal()"><i class="la la-plus"></i></button>
-<!-- <script src="vue.js"></script> -->
+  <button class="open-modal" onclick="openModal()"><i class="la la-plus"></i></button>
+  <!-- <script src="vue.js"></script> -->
   <!-- <script src="vue-script.js"></script> -->
   <script src="budget.js" type="text/javascript"></script>
 </body>
